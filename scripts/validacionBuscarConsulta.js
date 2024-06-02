ddccd7c4e2f18803274a9b2cc0885a3a64476f80
Joao Nuñez <104280636+joaonunez@users.noreply.github.com>
2024-06-02 01:37:27 -0400

    Update validacionBuscarConsulta.js

--- scripts/validacionBuscarConsulta.js
+++ scripts/validacionBuscarConsulta.js
@@ -1,21 +1,35 @@
 (function () {
     'use strict'
-  
-    // Selecciona todos los formularios que tienen la clase 'needs-validation'
-    const forms = document.querySelectorAll('.needs-validation2')
-  
-    // Recorre todos los formularios y evita el envío en caso de que sean inválidos
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                } else {
-                    event.preventDefault(); // Prevenir la recarga de la página
-                    buscarConsulta(); // Ejecutar tu función personalizada
-                }
-                form.classList.add('was-validated')
-            }, false)
-        })
-  })()
+
+    // Selecciona todos los formularios que tienen la clase 'needs-validation2'
+    const forms = document.querySelectorAll('.needs-validation2');
+
+    // Selecciona el botón de búsqueda de consulta
+    const buscarConsultaBtn = document.getElementById('buscarConsultaBtn');
+
+    // Función de validación personalizada
+    const validateForm = (form) => {
+        if (!form.checkValidity()) {
+            form.classList.add('was-validated');
+            return false;
+        }
+        return true;
+    };
+
+    // Añade el evento 'click' al botón de búsqueda de consulta
+    buscarConsultaBtn.addEventListener('click', function () {
+        // Recorre todos los formularios con la clase 'needs-validation2'
+        Array.prototype.slice.call(forms).forEach(function (form) {
+            // Validar el formulario
+            if (validateForm(form)) {
+                // Ejecutar la función personalizada 'buscarConsulta' si el formulario es válido
+                buscarConsulta();
+                // Abrir el modal si el formulario es válido
+                var myModal = new bootstrap.Modal(document.getElementById('verConsultas'), {
+                    keyboard: false
+                });
+                myModal.show();
+            }
+        });
+    });
+})();
